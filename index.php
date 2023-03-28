af7e5fcaa3782f0ed6a6c4b99722f752beed9c3a
Eddy Condori <leonlee342@gmail.com>
2023-03-27 22:46:49 -0400

    Comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php include('app/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body style="background-image: url('public/imagenes/piso-fondo.jpg');
+             background-repeat:no-repeat; ;
+             z-index: -3;
+             backgroun-size: 100vw 100vh" >
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <!-- Esto es el logo -->
+                <a class="navbar-brand" href="#">
+                    <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+                    FCYT-PARQUEO
+                </a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active" >
+                <a class="nav-link" href="#">CONTACTANOS</a>
+            </li>
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                PROMOCIONES
+                </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">MENSUALES</a>
+                    <a class="dropdown-item" href="#">SEMANALES</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">FICHAS</a>
+                </div>
+            </li>
+
+            </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+        </div>
+</nav>
+
+<div class="container">
+        <table class="table">
+            <tr>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>1</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>2</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>3</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>4</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>5</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>5</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>6</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>7</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>8</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>9</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+                <td width="100px">
+                    <p>
+                        <center>
+                            <h3><b>10</b></h3>
+                            <img src="public/imagenes/auto1.png" width="50px" alt="">
+                        </center>
+                    </p>
+                </td>
+            </tr>    
+        </table>
+
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="public/js/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="public/js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="public/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+ 
+</body>
+</html>
